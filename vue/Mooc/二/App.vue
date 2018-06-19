210cab332dbe2743ba4aff2f54768445029b8a16
liukang <529446209@qq.com>
2018-06-19 09:26:48 +0800

    重构代码

---
+++ vue/Mooc/二/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ hello }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data(){
+            return{
+                hello:'world'
+        }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
